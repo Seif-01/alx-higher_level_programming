cca7177291de077a8228698427ee715ffdeff065
Seif-01 <sifreyassin@gmail.com>
2024-03-13 14:59:05 -0300

    task 2 hot fix 4

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') +1):
-    print(chr(i), end="")
+output = " "
+for i in range(ord('a'), ord('z') +1,):
+    output += chr(i)
+    print(output, end="")
